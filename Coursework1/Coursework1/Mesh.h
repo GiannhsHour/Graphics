4f0a4e43d92ee67942c1523087d8c7b5cb751ef9
Giannhs <Giannhs@DESKTOP-PAOEACI>
2017-11-08 15:38:56 +0000

    first commit

---
+++ Coursework1/Coursework1/Mesh.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "OGLRenderer.h"
+
+ enum MeshBuffer {
+	 VERTEX_BUFFER, COLOUR_BUFFER, TEXTURE_BUFFER, MAX_BUFFER
+
+};
+class Mesh {
+public:
+	Mesh(void);
+	~Mesh(void);
+	virtual void Draw();
+	static Mesh * GenerateTriangle();
+	void SetTexture(GLuint tex) { texture = tex; }
+	GLuint GetTexture() { return texture; }
+	static Mesh * GenerateQuad();
+protected:
+	GLuint texture;
+	Vector2 * textureCoords;
+	void BufferData();
+	GLuint arrayObject;
+	GLuint bufferObject[MAX_BUFFER];
+	GLuint numVertices;
+	GLuint type;
+	Vector3 * vertices;
+	Vector4 * colours;
+};
